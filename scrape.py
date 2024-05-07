3144fe4422914d7db746b037718def133827a649
Cristy Wei <78058671+cristywei@users.noreply.github.com>
2024-05-07 13:05:29 -0500

    branch config

--- scrape.py
+++ scrape.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import requests
 from bs4 import BeautifulSoup
 
